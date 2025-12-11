9d5faf00d0c0dc7d629797b9c5f8d34872cd001f
kucheryulik-collab <kucheryulik@gmail.com>
2025-12-11 19:20:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -235,7 +235,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "Чтобы мои прогнозы точнее подстраивались под твой путь, "
         "напиши, пожалуйста, *дату рождения* в формате:\n"
         "`ДД.MM.ГГГГ`\n\n"
-        "Например: `23.06.1993`.\n\n"
+        "Например: `ДД.ММ.ГГГГ`.\n\n"
     )
 
     await update.message.reply_text(
